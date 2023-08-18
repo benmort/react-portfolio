d72092bfd026efb3ba507e6396bfd482cfd82894
benmort <benjaminmort@gmail.com>
2023-08-18 14:21:13 +1000

    gtm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
+import TagManager from 'react-gtm-module'
 import App from './app/App';
 import './index.css';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-N4XQ6R83'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
